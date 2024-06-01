0702164e2813a8df4f527a4590d89e9a3e2a157a
Sanukadg <sanukagamage@gmail.com>
2024-06-01 17:51:48 +0530

    Add: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a small react web app project created to learn myself how to work with APIs. 
+> "food app" can provide recipe ingredients and instructions.
+> it uses "Spoonacular" API : https://spoonacular.com/food-api/
---
+++ src/components/Search.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import styles from './search.module.css';
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+
+export default function Search({foodData, setFoodData}){
+  const [query,setQuery] = useState("pizza");
+
+  useEffect(() => {
+    async function fetchFood(){
+      const res = await fetch(`${URL}?query=${query}&apiKey=${import.meta.env.VITE_API_KEY}`);
+      const data = await res.json();
+      setFoodData(data.results);
+    }
+    fetchFood()
+  }, [query])
+
+  return(
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input} 
+        value={query}
+        onChange={(e)=>setQuery(e.target.value)} 
+        type="text"
+      />
+    </div>
+  );
+}
